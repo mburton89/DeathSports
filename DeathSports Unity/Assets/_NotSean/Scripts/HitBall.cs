e7fb03802d53641362f4752d8a01623dba069d16
Sean Mercer <60895318+NotSean725@users.noreply.github.com>
2021-01-14 18:45:58 -0600

    Did all of my Trello tasks
    - Can swing
    - Can hit ball
    - Ball goes flying

---
+++ DeathSports Unity/Assets/_NotSean/Scripts/HitBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBall : MonoBehaviour
+{
+    public Rigidbody Ball;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Try to see if I can make this part random
+        Ball.velocity = new Vector3(0, 15, 50);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DeathSports Unity/Assets/_NotSean/Scripts/RotatePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour
+{
+    public Vector3 directionToRotate;
+    public float rotationSpeed;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(directionToRotate * rotationSpeed * Time.deltaTime);
+        }
+        
+    }
+}
