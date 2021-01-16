7d8c1948b1bca02446701ecc0699380af4782334
Sean Mercer <60895318+NotSean725@users.noreply.github.com>
2021-01-16 10:10:16 -0600

    Tried to fix stuph

--- DeathSports Unity/Assets/_NotSean/Scripts/HitBall.cs
+++ DeathSports Unity/Assets/_NotSean/Scripts/HitBall.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class HitBall : MonoBehaviour
 {
-    //Use this without spawn script
     //public Rigidbody Ball;
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    //Ball.velocity = new Vector3(Random.Range(-15, 20), Random.Range(1, 15), Random.Range(1, 40));
-    //}
-
-
-    public Rigidbody Ball;
-
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("HIT");
-        Ball.velocity = new Vector3(0,1,10);
+        if(other.tag == "Ball")
+        {
+            other.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-15, 20), Random.Range(1, 15), Random.Range(1, 40));
+        }
+        //Ball.velocity = new Vector3(Random.Range(-15, 20), Random.Range(1, 15), Random.Range(1, 40));
     }
 }
 
