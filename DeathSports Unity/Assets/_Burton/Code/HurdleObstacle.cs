5d94461ec4c309aa17668dcde0a25931c4dcad3e
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-01-17 20:04:29 -0600

    Made Mini Hurdle Game

---
+++ DeathSports Unity/Assets/_Burton/Code/HurdleObstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurdleObstacle : MonoBehaviour
+{
+    private Vector3 _movementDirection;
+    private float _movementSpeed;
+
+    public void Init(int xDirection, float movementSpeed)
+    {
+        _movementDirection = new Vector3 (xDirection, 0, 0);
+        _movementSpeed = movementSpeed;
+    }
+
+    void Update()
+    {
+        transform.Translate((_movementDirection * _movementSpeed) * Time.deltaTime);
+    }
+}
---
+++ DeathSports Unity/Assets/_Burton/Code/HurdleSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurdleSpawner : MonoBehaviour
+{
+    [SerializeField] private HurdleObstacle _hurdlePrefab;
+    [SerializeField] private HurdleObstacle _raptorPrefab;
+    [SerializeField] private Transform _hurdleSpawnPoint;
+    [SerializeField] private Transform _raptorSpawnPoint;
+    [SerializeField] private float _spawnRate;
+    [SerializeField] private float _objectSpeed;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnObject());
+    }
+
+    private IEnumerator SpawnObject()
+    {
+        yield return new WaitForSeconds(_spawnRate);
+
+        int rand = Random.Range(0, 4);
+
+        if (rand == 0)
+        {
+            HurdleObstacle raptor = Instantiate(_raptorPrefab, _raptorSpawnPoint.position, _raptorSpawnPoint.rotation, _raptorSpawnPoint) as HurdleObstacle;
+            raptor.Init(1, _objectSpeed * 4);
+        }
+        else
+        {
+            HurdleObstacle hurdle = Instantiate(_hurdlePrefab, _hurdleSpawnPoint.position, _hurdleSpawnPoint.rotation, _hurdleSpawnPoint) as HurdleObstacle;
+            hurdle.Init(-1, _objectSpeed * 4);
+        }
+
+        StartCoroutine(SpawnObject());
+    }
+}
