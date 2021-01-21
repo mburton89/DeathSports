22f0610e4635514fb71476d6e97af213eb8ca7de
Namtaskic <nnnamman3@gmail.com>
2021-01-20 18:53:52 -0600

    Swimming Prototype
    Prototype DONE.

---
+++ DeathSports Unity/Assets/_Nguyen/Scenes/Swimming.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swimming : MonoBehaviour
+{
+    public KeyCode LeftArm;
+    public KeyCode RightArm;
+    public float swimspeed; 
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(LeftArm))
+        {
+            _rb.AddRelativeForce(Vector3.right * swimspeed);
+            //Twist Right
+            _rb.AddTorque(new Vector3(0, 1, 0));
+        }
+
+        else if (Input.GetKeyDown(RightArm))
+        {
+            _rb.AddRelativeForce(Vector3.right * swimspeed);
+            //Twist Left 
+            _rb.AddTorque(new Vector3(0, -1, 0));
+        }
+    }
+}
