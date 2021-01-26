5787c2a90a601a1a78587a95db1f5c2edcddcb3e
Matt Burton <matthewwburton@gmail.com>
2021-01-26 17:31:51 -0600

    Added awesome splodes to hurdles

--- DeathSports Unity/Assets/_Burton/Code/HurdleSpawner.cs
+++ DeathSports Unity/Assets/_Burton/Code/HurdleSpawner.cs
@@ -21,40 +21,13 @@ public class HurdleSpawner : MonoBehaviour
 
     private IEnumerator SpawnObject()
     {
-        //int rand1 = Random.Range(0, 3);
-
-        //if (rand1 == 2 && _canSpawnDouble)
-        //{
-        //    yield return new WaitForSeconds(_spawnRate / 2);
-        //    _canSpawnDouble = false;
-        //}
-        //else
-        //{
-        //    yield return new WaitForSeconds(_spawnRate);
-        //    _canSpawnDouble = true;
-        //}
-
-        //int rand2 = Random.Range(0, 4);
-
-        //if (rand2 == 0)
-        //{
-        //    HurdleObstacle raptor = Instantiate(_raptorPrefab, _raptorSpawnPoint.position, _raptorSpawnPoint.rotation, _raptorSpawnPoint) as HurdleObstacle;
-        //    raptor.Init(1, _objectSpeed * 4);
-        //}
-        //else
-        //{
-        //    HurdleObstacle hurdle = Instantiate(_hurdlePrefab, _hurdleSpawnPoint.position, _hurdleSpawnPoint.rotation, _hurdleSpawnPoint) as HurdleObstacle;
-        //    hurdle.Init(-1, _objectSpeed * 4);
-        //}
-
-        //StartCoroutine(SpawnObject());
-
         int rand = Random.Range(0, 6);
         if (rand == 0)
         {
             //spawn raptor
+            yield return new WaitForSeconds(_spawnRate / 2);
             HurdleObstacle raptor = Instantiate(_raptorPrefab, _raptorSpawnPoint.position, _raptorSpawnPoint.rotation, _raptorSpawnPoint) as HurdleObstacle;
-            raptor.Init(1, _objectSpeed * 4);
+            raptor.Init(1, _objectSpeed * 6);
         }
         else if (rand == 1 || rand == 2)
         {
---
+++ DeathSports Unity/Assets/_Burton/Code/HurdlesGameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HurdlesGameManager : MonoBehaviour
+{
+    public static HurdlesGameManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Restart()
+    {
+        StartCoroutine(RestartCo());
+    }
+
+    private IEnumerator RestartCo()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
