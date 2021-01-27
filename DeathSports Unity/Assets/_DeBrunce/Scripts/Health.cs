0e98e33d1e98f1e98581490937335ad9565a9c3f
Alexis <adebrunce@yahoo.com>
2021-01-27 17:27:09 -0600

    updated ui

---
+++ DeathSports Unity/Assets/_DeBrunce/Scripts/Health.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHealth;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+
+    private void Update()
+    {
+        if (health > numOfHealth)
+        {
+            health = numOfHealth;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHealth)
+            {
+                hearts[i].enabled = true; 
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+}
