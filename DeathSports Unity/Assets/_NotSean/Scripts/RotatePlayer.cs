8d4ab7e40b6e6c4107bd3662bf61f01de5d5860e
Sean Mercer <60895318+NotSean725@users.noreply.github.com>
2021-02-03 22:36:03 -0600

    Joe Baseball now resets his position as well as his power meter bar

--- DeathSports Unity/Assets/_NotSean/Scripts/RotatePlayer.cs
+++ DeathSports Unity/Assets/_NotSean/Scripts/RotatePlayer.cs
@@ -7,9 +7,27 @@ public class RotatePlayer : MonoBehaviour
     // Use this without respawn balls
     public Vector3 directionToRotate;
     public float rotationSpeed;
+    public float delayTime = 1.0f;
 
     // If anyone looks at this code, I don't know what it means either.
     IEnumerator RotateMe(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+
+        // Delay until Joe Baseball goes back to original position
+        yield return new WaitForSeconds(delayTime);
+
+        // Joe Baseball's original position
+        transform.rotation = Quaternion.Euler(new Vector3(0, 50, 0));
+    }
+
+    IEnumerator unRotateMe(Vector3 byAngles, float inTime)
     {
         var fromAngle = transform.rotation;
         var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
@@ -24,15 +42,18 @@ public class RotatePlayer : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
-            //transform.Rotate(directionToRotate * rotationSpeed * Time.deltaTime);
-
             // -90 is the rotation, 0.3f is the speed of the rotation
             StartCoroutine(RotateMe(Vector3.up * -90, 0.3f));
         }
+    }
 
-        /*if (Input.GetMouseButtonUp(0))
-        {
-            transform.Rotate(directionToRotate * rotationSpeed * Time.deltaTime);
-        }*/
+    IEnumerator resetPlayerPosition()
+    {
+        yield return new WaitForSeconds(delayTime);
+    }
+
+    void resetPlayer()
+    {
+        transform.Rotate(directionToRotate * rotationSpeed * Time.deltaTime);
     }
 }
