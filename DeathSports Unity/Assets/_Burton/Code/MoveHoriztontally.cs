873d414a5b9a735e44d93f0df572e7b466d2047b
Matt Burton <matthewwburton@gmail.com>
2021-02-10 11:17:24 -0600

    Update MoveHoriztontally.cs

--- DeathSports Unity/Assets/_Burton/Code/MoveHoriztontally.cs
+++ DeathSports Unity/Assets/_Burton/Code/MoveHoriztontally.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class MoveHoriztontally : MonoBehaviour
 {
-    public Vector3 _movementDirection;
-    public float _movementSpeed;
+    public Vector3 movementDirection;
+    public float movementSpeed;
     public float maxXPosition;
 
     public void Init(int xDirection, float movementSpeed)
     {
-        _movementDirection = new Vector3(xDirection, 0, 0);
-        _movementSpeed = movementSpeed;
+        movementDirection = new Vector3(xDirection, 0, 0);
+        this.movementSpeed = movementSpeed;
     }
 
     void Update()
     {
-        transform.Translate((_movementDirection * _movementSpeed) * Time.deltaTime);
+        transform.Translate((movementDirection * movementSpeed) * Time.deltaTime);
         if (transform.position.x > maxXPosition || transform.position.x < -maxXPosition)
         {
             Destroy(gameObject);
