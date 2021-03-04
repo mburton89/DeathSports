fe29b60b1bfdb28d57fe22cc5256674d8169c830
WUStudent <wustudent@webster.edu>
2021-03-03 19:44:10 -0600

    Added swim things

--- DeathSports Unity/Assets/_Nguyen/Code/TNTSpawner.cs
+++ DeathSports Unity/Assets/_Nguyen/Code/TNTSpawner.cs
@@ -6,18 +6,22 @@ public class TNTSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _tntPrefab;
     [SerializeField] private int _numberOfTNTToSpawn;
-    [SerializeField] private int _maxX;
-    [SerializeField] private int _maxY;
-    [SerializeField] private int _z; //Set this to whatever Z TNT in the scene is set to
+    [SerializeField] private float _maxX;
+    [SerializeField] private float _y;
+    [SerializeField] private float _maxZ; //Set this to whatever Z TNT in the scene is set to
 
     void Start()
     {
         for (int i = 0; i < _numberOfTNTToSpawn; i++)
         {
-            float randX = Random.Range(-_maxX, _maxX);
-            float randY = Random.Range(-_maxY, _maxY);
-            Vector3 spawnPosition = new Vector3(randX, randY, _z);
-            Instantiate(_tntPrefab, spawnPosition, transform.rotation, null);
+            float randX = Random.Range(-_maxX, _maxX); //change -maxX to zero
+            float randZ = Random.Range(-_maxZ, _maxZ); //change -maxZ to zero
+            Vector3 spawnPosition = new Vector3(randX, _y, randZ);
+            GameObject tnt = Instantiate(_tntPrefab, spawnPosition, transform.rotation, null) as GameObject;
+
+            float randRotation = Random.Range(0f, 360f);
+            tnt.transform.eulerAngles = new Vector3(0, randRotation, 0);
+
         }
     }
 }
