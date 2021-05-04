74d296695e333558c7858c799e1b94af7743602d
Matt Burton <matthewwburton@gmail.com>
2021-05-04 16:57:31 -0500

    credits

--- DeathSports Unity/Assets/_Burton/Code/SimpleMainMenu.cs
+++ DeathSports Unity/Assets/_Burton/Code/SimpleMainMenu.cs
@@ -7,12 +7,18 @@ public class SimpleMainMenu : MonoBehaviour
     public Button hurdlesButton;
     public Button baseballButton;
     public Button swimmingButton;
+    public Button creditsButton;
+    public Button backButton;
+    public GameObject menuContainer;
+    public GameObject creditsContainer;
 
     void Start()
     {
         hurdlesButton.onClick.AddListener(LoadHurdles);
         baseballButton.onClick.AddListener(LoadBaseball);
         swimmingButton.onClick.AddListener(LoadSwimming);
+        creditsButton.onClick.AddListener(OpenCredits);
+        backButton.onClick.AddListener(GoBack);
     }
 
     void LoadHurdles()
@@ -29,5 +35,17 @@ public class SimpleMainMenu : MonoBehaviour
     void LoadSwimming()
     {
         SceneManager.LoadScene(3);
+    }
+
+    void OpenCredits()
+    {
+        menuContainer.transform.localScale = Vector3.zero;
+        creditsContainer.transform.localScale = Vector3.one;
+    }
+
+    void GoBack()
+    {
+        menuContainer.transform.localScale = Vector3.one;
+        creditsContainer.transform.localScale = Vector3.zero;
     }
 }
